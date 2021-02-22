029e1418cd7113bc20efa29d296d34c4208e3d9c
indhu-github <indhusworkmail@gmail.com>
2021-02-22 13:12:36 +0530

    Updates

---
+++ precache-manifest.14e507f5d1ee89187ffbe4af8db2528d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ce03ccd7e74083b124bf5bb7410fec",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "c38adbce730fc7278eda",
+    "url": "/github-finder/static/css/main.68aed381.chunk.css"
+  },
+  {
+    "revision": "7403166f0cf9cde93d04",
+    "url": "/github-finder/static/js/2.4a0c70c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-finder/static/js/2.4a0c70c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38adbce730fc7278eda",
+    "url": "/github-finder/static/js/main.a16c3ede.chunk.js"
+  },
+  {
+    "revision": "189fbc883acbcb16644e",
+    "url": "/github-finder/static/js/runtime-main.c7dda347.js"
+  }
+]);
